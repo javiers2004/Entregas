e9f83a9564ddb33118b38c0eda5b5972201af671
js834 <js834@LAPTOP-VJR4VD4I>
2023-11-03 15:39:36 +0100

    entrega practica 5 ejercicios 1-7

---
+++ Entrega05/src/GestionTwitter.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.util.HashMap;
+
+public class GestionTwitter {
+	protected static HashMap<String, UsuarioTwitter> mapausuarios = new HashMap<String, UsuarioTwitter>();  //ORDENADOS POR ID
+	protected static HashMap<String, UsuarioTwitter> mapausuariosnick = new HashMap<String, UsuarioTwitter>();  //ORDENADOS POR NICK
+	
+	public static void main(String[] args) {
+		try {
+			// TODO Configurar el path y ruta del fichero a cargar
+			String fileName = "src/datareduced.csv";
+			CSV.processCSV( new File( fileName ),  mapausuarios, mapausuariosnick);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
